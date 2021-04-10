154cca28e797a3f5370dfea525839c6f5a803f8d
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-10 10:54:14 +0700

    use mvvm with databinding

--- app/src/main/java/io/github/pengdst/simplemvvm/MainActivity.java
+++ app/src/main/java/io/github/pengdst/simplemvvm/MainActivity.java
@@ -1,88 +1,30 @@
 package io.github.pengdst.simplemvvm;
 
+import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import io.github.pengdst.simplemvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edtAlas,edtTinggi,edtPanjang,edtLebar;
-    private Button btnHitungLuas,btnHitungKeliling;
-    private TextView tvHasilLuas,tvHasilKeliling;
     private MainViewModel viewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        edtAlas = findViewById(R.id.edt_alas);
-        edtTinggi = findViewById(R.id.edt_tinggi);
-        edtPanjang = findViewById(R.id.edt_panjang);
-        edtLebar= findViewById(R.id.edt_lebar);
-
-        tvHasilLuas = findViewById(R.id.tv_hasil_luas);
-        tvHasilKeliling = findViewById(R.id.tv_hasil_keliling);
-
-        btnHitungKeliling = findViewById(R.id.btn_hitung_keliling);
-        btnHitungLuas = findViewById(R.id.btn_hitung_luas);
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initViewModel();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        btnHitungLuas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (edtAlas.getText() != null && edtTinggi.getText() != null){
-                    Double alas = Double.valueOf(edtAlas.getText().toString());
-                    Double tinggi = Double.valueOf(edtTinggi.getText().toString());
-
-                    viewModel.hitungLuas(alas,tinggi);
-                }
-            }
-        });
-
-        btnHitungKeliling.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (edtPanjang.getText() != null && edtLebar.getText() != null){
-                    Double panjang = Double.valueOf(edtPanjang.getText().toString());
-                    Double lebar = Double.valueOf(edtLebar.getText().toString());
-
-                    viewModel.hitungKeliling(panjang,lebar);
-                }
-            }
-        });
-
-        viewModel.observeLuas().observe(this, new Observer<Double>() {
-            @Override
-            public void onChanged(Double luas) {
-                tvHasilLuas.setText(luas.toString());
-            }
-        });
-
-        viewModel.observeKeliling().observe(this, new Observer<Double>() {
-            @Override
-            public void onChanged(Double keliling) {
-                tvHasilKeliling.setText(keliling.toString());
-            }
-        });
     }
 
     private void initViewModel() {
         ViewModelProvider.Factory factory = new ViewModelProvider.NewInstanceFactory();
         viewModel = new ViewModelProvider(this, factory).get(MainViewModel.class);
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(this);
     }
 }
--- app/src/main/java/io/github/pengdst/simplemvvm/MainViewModel.java
+++ app/src/main/java/io/github/pengdst/simplemvvm/MainViewModel.java
@@ -1,30 +1,51 @@
 package io.github.pengdst.simplemvvm;
 
-import androidx.lifecycle.LiveData;
+import android.content.Context;
+import android.widget.Toast;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
-    private MutableLiveData<Double> luas = new MutableLiveData<>();
-    private MutableLiveData<Double> keliling = new MutableLiveData<>();
+    public MutableLiveData<String> etPanjang = new MutableLiveData<>();
+    public MutableLiveData<String> etLebar = new MutableLiveData<>();
 
-    public void hitungLuas(Double panjang, Double lebar){
-        Double hasil = panjang * lebar;
-        luas.postValue(hasil);
+    public MutableLiveData<String> keliling = new MutableLiveData<>();
+    public MutableLiveData<String> luas = new MutableLiveData<>();
+
+    public void hitungLuasPersegi(Context context){
+        if ((etPanjang == null || etPanjang.getValue() == null) || (etLebar == null || etLebar.getValue() == null)){
+            Toast.makeText(context, "Semua Field Harus Terisi", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Double panjang = Double.parseDouble(etPanjang.getValue());
+        Double lebar = Double.parseDouble(etLebar.getValue());
+
+        hitungLuasPersegi(panjang, lebar);
     }
 
-    public void hitungKeliling(Double panjang, Double lebar){
-        Double hasil = 2 * (panjang * lebar);
-        keliling.postValue(hasil);
+    public void hitungKelilingPersegi(Context context){
+
+        if ((etPanjang == null || etPanjang.getValue() == null) || (etLebar == null || etLebar.getValue() == null)){
+            Toast.makeText(context, "Semua Field Harus Terisi", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Double panjang = Double.valueOf(etPanjang.getValue());
+        Double lebar = Double.valueOf(etLebar.getValue());
+
+        hitungKelilingPersegi(panjang, lebar);
     }
 
-    public LiveData<Double> observeLuas(){
-        return luas;
+    public void hitungLuasPersegi(Double panjang, Double lebar){
+        double hasil = panjang * lebar;
+        luas.postValue(Double.toString(hasil));
     }
 
-    public LiveData<Double> observeKeliling(){
-        return keliling;
+    public void hitungKelilingPersegi(Double panjang, Double lebar){
+        double hasil = 2 * (panjang * lebar);
+        keliling.postValue(Double.toString(hasil));
     }
 
 }
